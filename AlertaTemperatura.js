845057092008c69cc2a52c91c72d52257dd603c9
Leonardo Gregorio de Sousa <leo.gregorio2010@gmail.com>
2025-11-14 23:26:40 -0300

    Add files via upload

---
+++ AlertaTemperatura.js
@@ -0,0 +1,11 @@
+function AlertaTemperatura(temp) {
+    if (temp < 10) {
+        return "Alerta de Frio"
+    } else if (temp >= 10 && temp <= 25) {
+        return "Temperatura Ideal"
+    } else {
+        return "Alerta de Calor"
+    }
+}
+
+console.log(AlertaTemperatura(27))
